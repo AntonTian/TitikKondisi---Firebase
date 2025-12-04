09b6018f1b8895052f9b92f4b8aebd35f0130c43
AntonTian <a.sebastiangunadi@gmail.com>
2025-12-04 14:14:28 +0700

    turned isProduction into false

--- functions/index.js
+++ functions/index.js
@@ -214,7 +214,7 @@ require("dotenv").config();
 const midtransClient = require("midtrans-client");
 
 const snap = new midtransClient.Snap({
-  isProduction: true, 
+  isProduction: false, 
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
